6e50ef9b8ac5737043942bb92999eba9d92c4cf9
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-05-12 13:14:56 +0530

    add updated certificates

--- app/noc/ECertificate/page.tsx
+++ app/noc/ECertificate/page.tsx
@@ -15,6 +15,8 @@ export default async function NocPage({
             ? "/content/noc/NOC24/SEM1/Ecertificates/127/noc24-ge24/Course/NPTEL24GE24S1263901755.pdf"
             : nptelValue === "NPTEL24GE24S1263902193"
             ? "/content/noc/NOC24/SEM1/Ecertificates/127/noc24-ge24/Course/NPTEL24GE24S1263902193.pdf"
+            : nptelValue === "NPTEL24GE24S1263901407"
+            ? "/content/noc/NOC24/SEM1/Ecertificates/127/noc24-ge24/Course/NPTEL24GE24S1263901407.pdf"
             : ""
         }
         target="_blank"
