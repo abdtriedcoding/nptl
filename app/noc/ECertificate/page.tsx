696c0d9c28efa7b4987c0a941799344ebe31856e
abdtriedcoding <siddabdullahofficial@gmail.com>
2024-11-28 12:56:16 +0530

    add new results

--- app/noc/ECertificate/page.tsx
+++ app/noc/ECertificate/page.tsx
@@ -9,16 +9,17 @@ export default async function NocPage({
     <div className="min-h-screen justify-center items-center flex">
       <a
         href={
-          nptelValue === "NPTEL24GE24S1263901353"
-            ? "/content/noc/NOC24/SEM1/Ecertificates/127/noc24-ge24/Course/NPTEL24GE24S1263901353.pdf"
-            : nptelValue === "NPTEL24GE24S1263901755"
-              ? "/content/noc/NOC24/SEM1/Ecertificates/127/noc24-ge24/Course/NPTEL24GE24S1263901755.pdf"
-              : nptelValue === "NPTEL24GE24S1263902193"
-                ? "/content/noc/NOC24/SEM1/Ecertificates/127/noc24-ge24/Course/NPTEL24GE24S1263902193.pdf"
-                : nptelValue === "NPTEL24GE24S1263901407"
-                  ? "/content/noc/NOC24/SEM1/Ecertificates/127/noc24-ge24/Course/NPTEL24GE24S1263901407.pdf"
-                  : nptelValue === "NPTEL24HS60S663901784"
-                    ? "/content/noc/NOC24/SEM1/Ecertificates/127/noc24-ge24/Course/NPTEL24HS60S663901784.pdf" : ""
+          nptelValue === "NPTEL24MG116S1051302119"
+            ? "/content/noc/NOC24/SEM1/Ecertificates/127/noc24-ge24/Course/NPTEL24MG116S1051302119.pdf"
+            : nptelValue === "NPTEL24HS60S663901784"
+            ? "/content/noc/NOC24/SEM1/Ecertificates/127/noc24-ge24/Course/NPTEL24HS60S663901784.pdf"
+            : nptelValue === "NPTEL24MG89S464300152"
+            ? "/content/noc/NOC24/SEM1/Ecertificates/127/noc24-ge24/Course/NPTEL24MG89S464300152.pdf"
+            : nptelValue === "NPTEL24MG116S1051302323"
+            ? "/content/noc/NOC24/SEM1/Ecertificates/127/noc24-ge24/Course/NPTEL24MG116S1051302323.pdf"
+            : nptelValue === "NPTEL24MG93S651304196"
+            ? "/content/noc/NOC24/SEM1/Ecertificates/127/noc24-ge24/Course/NPTEL24MG93S651304196.pdf"
+            : ""
         }
         target="_blank"
         className="inline-block bg-[#3070bf] text-white px-8 py-2 rounded-md text-center text-base font-times-new-roman"
